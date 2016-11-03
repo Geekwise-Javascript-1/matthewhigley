90e30a8c77b5fe99e420b7b814437a8e430266d6
Matthew Higley <matthewhigley@MattyBook-Pro.local>
2016-11-02 17:59:13 -0700

    day01

---
+++ day01/js/assignment.js
@@ -0,0 +1,22 @@
+/*
+Matthew Higley
+November 02, 2016
+JavaScript 1
+*/
+
+var a = 12;
+var b = parseInt('0x'+(30).toString(16));
+var c = 'Answer equals: ';
+
+var d = a + b;
+console.log(d);
+
+// var n1 = 12;
+// var n2 = 0x1e;
+// var s1 = "Answer equals: ";
+//
+// var n3 = n1 + n2;
+// var answer = s1 + n3;
+//
+// console.log(n3);
+// console.log(answer);
---
+++ day01/js/script.js
@@ -0,0 +1,75 @@
+//single line comment
+
+/*
+multi line comment
+
+
+
+
+still commented
+
+
+
+*/
+
+// console.log("Hello World");
+
+var firstName = "Pratima";
+// console.log(firstName);
+
+/*Strings*/
+var string1 = "this is a string"; // string literal
+var string2 = "I've done this before";  //mix and match quotes
+var string3 = new String("This is a string also"); // string object
+/*Numbers*/
+var number1 = 5; // integer 5
+var number2 = "5"; // string 5 <-- not a number!
+/*Floats*/
+var float1 = 2.75;
+var float2 = 245.45678;
+/*Boolean*/
+var boolean1 = true;
+var boolean2 = false;
+/*Array*/
+var array1 = ['bread', 'eggs', 'milk'];
+var array2 = ['matt', 6.4, false];
+// console.log(array2[1]); // output index 1 of array
+/*Object*/
+var object1 = {
+    name: 'matt',
+    height: 6.4,
+    caffinated: false
+};
+// console.log(object1.height);
+
+// Create two string variables: firstname & lastname
+// Create two number variables: bday-month & bday-date
+// Create two float variables: height & weight
+// Create two boolean variables: gender & pets
+// Create two array variables: 3 fav colors & 3 fav movies
+// Create two object variables: car (3 items) & yourself (3 items)
+// Console each.
+
+/* Variable names:
+    Underscored: variable_name
+    Numeric: variable2
+  ! Camelcase: variableName
+*/
+
+/*typeof keyword*/
+console.log(typeof boolean1);
+console.log(typeof array1);
+console.log(typeof number1);
+
+/*
+On the assignment.js file
+Create three variables.
+The first two should store the numbers:
+values 12
+and 30
+(for an extra challenge... convert 30 into a hexadecimal)
+The third variable should store the string "Answer equals: "
+Add the first two numeric variables together using the + operator, save this to a new variable.
+Concatenate the string from your third variable with the number from your fourth (new) variable. Save this to a new variable called "answer".
+Console log answer.
+*/
